73ca657a4968acd8f51c6d185c35ac18b6d60a8b
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 19:23:53 -0400

    nums

---
+++ Giraffe/nums.py
@@ -0,0 +1,25 @@
+print(-5141.4756345)
+
+print(265+58)
+
+print(10%3)
+
+num = 1234
+print(str(num)+" favvvvv")
+
+nee=-45
+print (abs(nee))
+
+print(pow(2,3))
+
+print(max(41584,4875))
+print(min(41584,4875))
+
+print(round(145.526))
+
+from math import *
+
+print(floor(4815.6))
+print(ceil(4815.6))
+
+print(sqrt(25))
