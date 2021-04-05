f44b1911bc97f1380adccd25d6283ca60046a93c
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 19:14:39 -0400

    strings

---
+++ Giraffe/strings.py
@@ -0,0 +1,20 @@
+print("string\nstring\"")
+
+sre = "sdfhjdafsg"
+print(sre+" whats this")
+
+tre = "sdjkfnNDJKSFGNmfsdjk"
+print(tre.upper())
+print(tre.lower())
+print(sre.islower())
+
+print(tre.upper().isupper())
+
+print(len(sre))
+print(len(tre))
+
+print(sre[4])
+
+print(tre.index("N"))
+
+print(sre.replace("sdf","huh"))
