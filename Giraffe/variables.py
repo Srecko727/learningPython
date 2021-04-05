bc35a7e530d3b61f384a39d3007b3bca9494ce95
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 19:05:20 -0400

    understadning variables

---
+++ Giraffe/variables.py
@@ -0,0 +1,10 @@
+charName = "srecko"
+charAge = "78"
+
+print("there was a dude named " +charName+", ")
+print("he was " +charAge+ " years old. ")
+
+charName = "bob"
+charAge = "85"
+print("he liked the name "+charName+", ")
+print("but he didnt like being "+charAge+".")
