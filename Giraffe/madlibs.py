03905c8b71f8692360e1502e5aa66141fcf50955
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 19:35:06 -0400

    simple madlib

---
+++ Giraffe/madlibs.py
@@ -0,0 +1,8 @@
+color = input("enter a color: ")
+pluralNoun = input("enter a plural noun: ")
+celeb = input("enter a celebrity: ")
+
+
+print("Roses are "+color)
+print(pluralNoun+" are blue")
+print("I love "+celeb)
