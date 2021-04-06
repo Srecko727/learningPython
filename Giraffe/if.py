a13caad285a5b04718c460d11617003085250c40
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 20:29:17 -0400

    if statement basics

---
+++ Giraffe/if.py
@@ -0,0 +1,9 @@
+isMale = True
+tall = False
+
+if isMale and tall:#and can be or as well
+    print("tall or males")
+elif isMale and not(tall):
+    print("short male")
+else:
+    print("short or female")
