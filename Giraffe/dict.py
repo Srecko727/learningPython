ed26dca0933943386c2f9ed49f03bf09111d9205
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-05 20:52:05 -0400

    dictionaries

---
+++ Giraffe/dict.py
@@ -0,0 +1,10 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    #...
+}
+
+print(monthConversions["Jan"])
+#or
+print(monthConversions.get("h","not a valid month"))
