dbd1df5a132aa0c5edd1e6a473cec4c8f78f11a0
Srecko727 <sreckomarkovic@ufl.edu>
2021-04-07 22:12:53 -0400

    for loops

---
+++ forLoop.py
@@ -0,0 +1,15 @@
+
+for letter in "oscar is the best":
+    print(letter)
+
+   
+homies = ["raph","grace","soph"]
+for letter in homies:
+    print(letter)
+
+for index in range(6,8):
+    print(index)
+
+
+for index in range(len(homies)):
+    print(homies[index])
